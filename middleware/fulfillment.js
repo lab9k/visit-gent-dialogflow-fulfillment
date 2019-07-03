ca3440028ad4667a782ee1bdb817e8304b2eebd6
Jefwillems <jefwillems100@gmail.com>
2019-07-03 14:58:32 +0200

    add first intent

--- middleware/fulfillment.js
+++ middleware/fulfillment.js
@@ -1,1 +1,15 @@
-module.exports = (ctx) => { ctx.body = 'hello world'; };
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+function events(agent) {
+  agent.add('I will look for events now');
+}
+
+const intentMap = new Map();
+intentMap.set('bot.events', events);
+
+
+module.exports = (ctx) => {
+  const { request, response } = ctx;
+  const agent = new WebhookClient({ request, response });
+  agent.handleRequest(intentMap);
+};
