d005924db393e660bedfc8284b14cea6d76ae3cb
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-04 12:46:55 +0200

    response events

--- src/intents/bot.events.js
+++ src/intents/bot.events.js
@@ -5,6 +5,6 @@ module.exports = {
   key: 'bot.events',
   handler(agent) {
     agent.add('Received your request for events successfully!');
-    fetch().then(res => agent.add(res));
+    agent.add(fetch());
   },
 };
