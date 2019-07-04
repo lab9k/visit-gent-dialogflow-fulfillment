1f3fd20dfbdcaed3225c7c304b85a3946a4c8359
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-04 13:30:24 +0200

    response

--- src/intents/bot.events.js
+++ src/intents/bot.events.js
@@ -4,7 +4,7 @@ const fetch = require('../../api/SparqlApi');
 module.exports = {
   key: 'bot.events',
   handler(agent) {
-    agent.add('Received your request for events successfully!');
+    // agent.add('Received your request for events successfully!');
     fetch().then(res => agent.add(res));
     // agent.add(`Events: ${fetch()}`);
   },
