32561d223669e5c494b389d634e2211b1f5d45ab
Jefwillems <jefwillems100@gmail.com>
2019-07-04 13:33:01 +0200

    merge

--- src/intents/bot.events.js
+++ src/intents/bot.events.js
@@ -4,10 +4,6 @@ const fetch = require('../../api/SparqlApi');
 module.exports = {
   key: 'bot.events',
   handler(agent) {
-<<<<<<< HEAD
-    // agent.add('Received your request for events successfully!');
-    fetch().then(res => agent.add(res));
-=======
     agent.add('Received your request for events successfully!');
     fetch();
     const fetched = fetch();
@@ -15,7 +11,6 @@ module.exports = {
       console.log('bot.events: result', res);
       agent.add(res);
     });
->>>>>>> origin/master
     // agent.add(`Events: ${fetch()}`);
   },
 };
