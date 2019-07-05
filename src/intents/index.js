874d7d530608c1e1e1dc9c18ade335dc442f2641
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-05 09:35:28 +0200

    test

--- src/intents/index.js
+++ src/intents/index.js
@@ -1,10 +1,12 @@
 const intentMap = new Map();
 const events = require('./bot.events');
-// const attractions = require('./bot.attractions');
+const attractions = require('./bot.attractions');
 
 [events].forEach((el) => {
   intentMap.set(el.key, el.handler);
   console.log(el.key);
+  console.log(el.handler);
 });
+
 
 module.exports = intentMap;
