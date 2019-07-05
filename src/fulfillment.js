1a2f3fd799f8e9a789a5f924fc616b49f1a53d34
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-05 14:34:06 +0200

    test get language code

--- src/fulfillment.js
+++ src/fulfillment.js
@@ -3,7 +3,7 @@ const { WebhookClient } = require('dialogflow-fulfillment');
 const intents = require('./intents');
 
 module.exports = (request, response) => {
-  console.log(request.languageCode);
+  console.log(request.responseId);
   const agent = new WebhookClient({ request, response });
   agent.handleRequest(intents);
 };
