1f92d9b150778c3fffc180db8c3866424f55c505
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-05 11:55:23 +0200

    bug image attractions

--- src/models/AttractionCard.js
+++ src/models/AttractionCard.js
@@ -3,7 +3,8 @@ const { Card } = require('dialogflow-fulfillment');
 module.exports = class AttractionCard extends Card {
   constructor(binding) {
     super(binding.name.value);
-    this.setImage(binding.imagesList.value[0]);
+    // this.setImage(JSON.stringify(binding.imagesList).split(',', 1));
+    this.setImage(binding.imagesList.value.split(',')[0]);
     this.setText(binding.description.value);
     this.setButton({ text: 'Go to website.', url: binding.page.value });
   }
