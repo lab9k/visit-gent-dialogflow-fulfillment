acf25a60ab695312e30784fd3c8632c2f852d00e
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-08 11:36:12 +0200

    delete category

--- src/intents/bot.attractions.category.js
+++ src/intents/bot.attractions.category.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-underscore-dangle */
+
+const i18n = require('i18n');
 
 module.exports = {
   key: 'bot.attractions',
   handler(agent) {
-    agent.add('test received');
+    agent.add(i18n.__('Looking for attractions'));
   },
 };
