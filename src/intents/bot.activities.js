ac5fa822a12c940b8d003a801a3b8e3a6da31bf5
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-09 16:44:12 +0200

    delete set context

--- src/intents/bot.activities.js
+++ src/intents/bot.activities.js
@@ -8,8 +8,5 @@ module.exports = {
     agent.add(`${i18n.__('What kind of activity would you like to do')}?`);
     agent.add(new Suggestion(`${i18n.__('Events')} ${agent.context.get('time').parameters.time}`));
     agent.add(new Suggestion(i18n.__('Attraction')));
-    const context = agent.context.get('time');
-    console.log(context);
-    agent.context.set(context);
   },
 };
