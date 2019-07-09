ebbf3847f1cafb80a97f1c219fbe2e71f64cee50
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-09 16:31:19 +0200

    context

--- src/intents/bot.activities.js
+++ src/intents/bot.activities.js
@@ -6,7 +6,7 @@ module.exports = {
   key: 'bot.attractions',
   handler(agent) {
     agent.add(`${i18n.__('What kind of activity would you like to do')}?`);
-    agent.add(new Suggestion(i18n.__('Event')));
+    agent.add(new Suggestion(`${i18n.__('Event')} ${agent.context.get('time').parameters.time}`));
     agent.add(new Suggestion(i18n.__('Attraction')));
   },
 };
