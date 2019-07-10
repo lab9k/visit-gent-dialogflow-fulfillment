0789b77ab30d3d6fbdc4f3d5df9f1f51a31137c7
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-10 14:40:51 +0200

    handler

--- src/intents/index.js
+++ src/intents/index.js
@@ -3,10 +3,12 @@ const events = require('./bot.events');
 const attractions = require('./bot.attractions');
 const attractionsCategory = require('./bot.attractions.category');
 const activities = require('./bot.activities');
+const location = require('./bot.location');
 
 intentMap.set('bot.events', events.handler);
 intentMap.set('bot.attractions', attractions.handler);
 intentMap.set('bot.attractions.category', attractionsCategory.handler);
 intentMap.set('bot.activities', activities.handler);
+intentMap.set('bot.location', location.handler);
 
 module.exports = intentMap;
