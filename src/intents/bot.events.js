ab01107e73ddd45806fd24df6aff92e11f59f23b
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-10 11:02:31 +0200

    test list

--- src/intents/bot.events.js
+++ src/intents/bot.events.js
@@ -4,44 +4,39 @@ const { Payload } = require('dialogflow-fulfillment');
 const { fetchEvents } = require('../../api/SparqlApi');
 const EventCard = require('../models/EventCard');
 
-function list() {
-  const facebookJson = {
-    template_type: 'list',
-    top_element_style: 'compact',
-    elements: [
-      {
-        title: 'Classic T-Shirt Collection',
-        subtitle: 'See all our colors',
-        image_url: 'https://peterssendreceiveapp.ngrok.io/img/collection.png',
-        buttons: [
-          {
-            title: 'View',
-            type: 'web_url',
-            url: 'https://peterssendreceiveapp.ngrok.io/collection',
-            messenger_extensions: true,
-            webview_height_ratio: 'tall',
-            fallback_url: 'https://peterssendreceiveapp.ngrok.io/',
-          },
-        ],
-      },
-    ],
-  };
-
-  const payload = new Payload(
-    'facebook',
-    facebookJson,
-    true,
-    true,
-  );
-
-  return payload;
-}
-
 module.exports = {
   key: 'bot.events',
   handler(agent) {
     agent.add(i18n.__('Looking for events'));
-    agent.add(`List: ${list()}`);
+
+    const facebookJson = {
+      template_type: 'list',
+      top_element_style: 'compact',
+      elements: [
+        {
+          title: 'Classic T-Shirt Collection',
+          subtitle: 'See all our colors',
+          image_url: 'https://peterssendreceiveapp.ngrok.io/img/collection.png',
+          buttons: [
+            {
+              title: 'View',
+              type: 'web_url',
+              url: 'https://peterssendreceiveapp.ngrok.io/collection',
+              messenger_extensions: true,
+              webview_height_ratio: 'tall',
+              fallback_url: 'https://peterssendreceiveapp.ngrok.io/',
+            },
+          ],
+        },
+      ],
+    };
+
+    agent.add(new Payload(
+      'facebook',
+      facebookJson,
+      true,
+      true,
+    ));
     const fetched = fetchEvents(agent.parameters.time);
     return fetched.then((res) => {
       // return top 3 events
