bfaa223210ca2b651b6b61dd5b11e6d70916f47b
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-10 14:36:55 +0200

    ask location permission

--- src/intents/bot.location.js
+++ src/intents/bot.location.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-underscore-dangle */
+const { Permission } = require('actions-on-google');
 
 module.exports = {
   key: 'bot.location',
   handler(agent) {
-    agent.ask('DEVICE_PRECISE_LOCATION');
+    agent.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
+    return agent.ask(new Permission({
+      context: 'to locate you',
+      permissions: agent.data.requestedPermission,
+    }));
   },
 };
