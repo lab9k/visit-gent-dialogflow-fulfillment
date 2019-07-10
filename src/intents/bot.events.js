f70dca88837ca38b5a0d9a5a6ae869830fb6c96f
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-10 11:45:54 +0200

    delete list

--- src/intents/bot.events.js
+++ src/intents/bot.events.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-underscore-dangle */
 const i18n = require('i18n');
-const { Payload } = require('dialogflow-fulfillment');
 const { fetchEvents } = require('../../api/SparqlApi');
 const EventCard = require('../models/EventCard');
 
@@ -8,40 +7,6 @@ module.exports = {
   key: 'bot.events',
   handler(agent) {
     agent.add(i18n.__('Looking for events'));
-
-    const facebookJson = {
-      template_type: 'generic',
-      elements: [
-        {
-          title: 'Welcome!',
-          image_url: 'https://petersfancybrownhats.com/company_image.png',
-          subtitle: 'We have the right hat for everyone.',
-          default_action: {
-            type: 'web_url',
-            url: 'https://petersfancybrownhats.com/view?item=103',
-            webview_height_ratio: 'tall',
-          },
-          buttons: [
-            {
-              type: 'web_url',
-              url: 'https://petersfancybrownhats.com',
-              title: 'View Website',
-            }, {
-              type: 'postback',
-              title: 'Start Chatting',
-              payload: 'DEVELOPER_DEFINED_PAYLOAD',
-            },
-          ],
-        },
-      ],
-    };
-
-    agent.add(new Payload(
-      'FACEBOOK',
-      facebookJson,
-      true,
-      true,
-    ));
     const fetched = fetchEvents(agent.parameters.time);
     return fetched.then((res) => {
       // return top 3 events
