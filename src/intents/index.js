763f1462bb7b7eced17febc12aa02e29dca3eef5
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-16 13:44:59 +0200

    bug

--- src/intents/index.js
+++ src/intents/index.js
@@ -1,12 +1,10 @@
 const intentMap = new Map();
 const events = require('./bot.events');
-const attractionsCategory = require('./bot.attractions.category');
 const activities = require('./bot.activities');
 const attractionsSubject = require('./bot.attractions.subject');
 const attractionsSubjectLocation = require('./bot.attractions.subject.location');
 
 intentMap.set('bot.events', events.handler);
-intentMap.set('bot.attractions.category', attractionsCategory.handler);
 intentMap.set('bot.activities', activities.handler);
 intentMap.set('bot.attractions.subject - no', attractionsSubject.handler);
 intentMap.set('bot.attractions.subject.location', attractionsSubjectLocation.handler);
