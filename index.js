ffce15766802433094b7a23299ad8d25cb7e48d1
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-16 08:57:45 +0200

    exceptions location

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const i18n = require('i18n');
 const bodyParser = require('body-parser');
-const rp = require('request-promise');
+const requestPromise = require('request-promise');
 const fulfillment = require('./src/fulfillment');
 
 const { fetchAttractions, fetchEvents } = require('./api/SparqlApi');
@@ -21,31 +21,38 @@ i18n.configure({
 
 function requestLocation(location) {
   const apiLocation = location.replace(/ /g, '+');
-  return rp(`https://eu1.locationiq.com/v1/search.php?key=0a580bdfff78da&q=${apiLocation}&format=json`);
+  return requestPromise(`https://eu1.locationiq.com/v1/search.php?key=0a580bdfff78da&q=${apiLocation}&format=json`)
+    .catch(e => console.log(e.message));
 }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 
-  fetchEvents('Today').then(res => console.log(res));
-
-  const fetchedLocation = requestLocation('oude houtlei 117 gent');
+  // fetchEvents('Today').then(res => console.log(res));
+  const fetchedLocation = requestLocation('gjgjgjgjgjgjg');
   const fetchedAttractions = fetchAttractions('Cafés');
   Promise.all([fetchedLocation, fetchedAttractions]).then((res) => {
-    const location = JSON.parse(res[0])[0];
-    const latitude = parseFloat(location.lat);
-    const longitude = parseFloat(location.lon);
-    const attractions = res[1];
-    let i;
-    const radius = 0.001;
-    for (i = 0; i < attractions.length; i += 1) {
-      const loc = attractions[i].asWKT.value.replace('POINT(', '').replace(')', '').split(' ');
-      if ((parseFloat(loc[0]) < (longitude + radius)
+    if (res[0] !== undefined) {
+      const location = JSON.parse(res[0])[0];
+      console.log(location);
+      if (!location.display_name.includes('Gent')) {
+        console.log('Location not found in Ghent');
+      } else {
+        const latitude = parseFloat(location.lat);
+        const longitude = parseFloat(location.lon);
+        const attractions = res[1];
+        let i;
+        const radius = 0.001;
+        for (i = 0; i < attractions.length; i += 1) {
+          const loc = attractions[i].asWKT.value.replace('POINT(', '').replace(')', '').split(' ');
+          if ((parseFloat(loc[0]) < (longitude + radius)
       && parseFloat(loc[0]) > (longitude - radius))
       && (parseFloat(loc[1]) < (latitude + radius)
       && parseFloat(loc[1]) > (latitude - radius))) {
-        // console.log(attractions[i].name);
+            console.log(attractions[i].name);
+          }
+        }
       }
     }
   });
--- src/intents/bot.attractions.subject.location.js
+++ src/intents/bot.attractions.subject.location.js
@@ -4,40 +4,49 @@ const { fetchAttractions } = require('../../api/SparqlApi');
 const AttractionCard = require('../models/AttractionCard');
 
 
-function requestLocation(location) {
+function requestLocation(location, agent) {
   const apiLocation = `${location.replace(/ /g, '+')}+Gent`;
   return rp(`https://eu1.locationiq.com/v1/search.php?key=0a580bdfff78da&q=${apiLocation}&format=json`)
-    .catch(e => console.log(e));
+    .catch((e) => {
+      console.log(e.message);
+      agent.add('Location not found;');
+    });
 }
 
 module.exports = {
   key: 'bot.attractions.subject.location',
   handler(agent) {
     agent.add('Looking for attractions');
-    const fetchedLocation = requestLocation(agent.parameters.address);
+    const fetchedLocation = requestLocation(agent.parameters.address, agent);
     const fetchedAttractions = fetchAttractions(agent.context.get('botattractionssubject-followup').parameters.subject);
     const fetched = Promise.all([fetchedLocation, fetchedAttractions]);
 
     return fetched.then((res) => {
-      const location = JSON.parse(res[0])[0];
-      const latitude = parseFloat(location.lat);
-      const longitude = parseFloat(location.lon);
-      const attractions = res[1];
-      let i;
-      let counter = 0;
-      const radius = 0.001;
-      for (i = 0; i < attractions.length; i += 1) {
-        const loc = attractions[i].asWKT.value.replace('POINT(', '').replace(')', '').split(' ');
-        if ((parseFloat(loc[0]) < (longitude + radius)
+      if (res[0] !== undefined) {
+        const location = JSON.parse(res[0])[0];
+        if (!location.display_name.includes('Gent')) {
+          agent.add('Location not found in Ghent');
+        } else {
+          const latitude = parseFloat(location.lat);
+          const longitude = parseFloat(location.lon);
+          const attractions = res[1];
+          let i;
+          let counter = 0;
+          const radius = 0.001;
+          for (i = 0; i < attractions.length; i += 1) {
+            const loc = attractions[i].asWKT.value.replace('POINT(', '').replace(')', '').split(' ');
+            if ((parseFloat(loc[0]) < (longitude + radius)
       && parseFloat(loc[0]) > (longitude - radius))
       && (parseFloat(loc[1]) < (latitude + radius)
       && parseFloat(loc[1]) > (latitude - radius))) {
-          counter += 1;
-          agent.add(new AttractionCard(attractions[i]));
+              counter += 1;
+              agent.add(new AttractionCard(attractions[i]));
+            }
+          }
+          if (counter === 0) {
+            agent.add('No attractions found');
+          }
         }
-      }
-      if (counter === 0) {
-        agent.add('No attractions found');
       }
     });
   },
