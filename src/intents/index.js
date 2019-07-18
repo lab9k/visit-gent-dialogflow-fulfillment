ffbc7d891f5532502095dcc94c0a45206bead158
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-18 14:22:24 +0200

    direct access to events

--- src/intents/index.js
+++ src/intents/index.js
@@ -5,6 +5,7 @@ const attractionsSubject = require('./bot.attractions.subject');
 const attractionsSubjectLocation = require('./bot.attractions.subject.location');
 
 intentMap.set('bot.events', events.handler);
+intentMap.set('bot.events.direct', events.handler);
 intentMap.set('bot.activities', activities.handler);
 intentMap.set('bot.attractions.subject - no', attractionsSubject.handler);
 intentMap.set('bot.attractions.subject.location', attractionsSubjectLocation.handler);
