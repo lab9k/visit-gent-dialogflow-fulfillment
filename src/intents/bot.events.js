d7749666780ef61aee789005d06d61e5dd2c7bd2
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-18 11:34:47 +0200

    slot filling

--- src/intents/bot.events.js
+++ src/intents/bot.events.js
@@ -7,38 +7,17 @@ const EventCard = require('../models/EventCard');
 module.exports = {
   key: 'bot.events',
   handler(agent) {
-    const fetched = fetchEvents('2019-07-18T12:00:00+02:00');
-    return fetched.then((res) => {
-      console.log('Fetched');
-      // return top 3 events
-      if (res.length < 1) {
-        agent.add(i18n.__('No events found'));
-      } else {
-        agent.add(`${i18n.__('Top 3 events')}: `);
-        let i;
-        let card;
-        for (i = 0; i < res.length; i += 1) {
-          card = new EventCard(res[i]);
-          agent.add(card);
-        }
-      }
-    });
-
-    /* console.log(agent.context.get('time'));
     if (agent.context.get('time').parameters.time === '' && agent.parameters.eventTime === '') {
       agent.add('What day are you looking for events?');
-      return new Promise();
+      return null;
     }
     let time;
     if (agent.context.get('time').parameters.time !== '') {
-      console.log('Time of context');
       ({ time } = agent.context.get('time').parameters);
     } else {
-      console.log('Time of parameters');
       time = agent.parameters.eventTime;
     }
     agent.add(i18n.__('Looking for events'));
-    console.log(time);
     const fetched = fetchEvents(time);
     return fetched.then((res) => {
       console.log('Fetched');
@@ -54,6 +33,6 @@ module.exports = {
           agent.add(card);
         }
       }
-    }); */
+    });
   },
 };
