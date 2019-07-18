0e1c750a4320c3fadf4ae5d9ade83961970479fc
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-07-18 11:41:49 +0200

    slot filling

--- src/intents/bot.events.js
+++ src/intents/bot.events.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
 const i18n = require('i18n');
 const { fetchEvents } = require('../../api/SparqlApi');
@@ -9,30 +10,31 @@ module.exports = {
   handler(agent) {
     if (agent.context.get('time').parameters.time === '' && agent.parameters.eventTime === '') {
       agent.add('What day are you looking for events?');
-      return null;
-    }
-    let time;
-    if (agent.context.get('time').parameters.time !== '') {
-      ({ time } = agent.context.get('time').parameters);
+      // return null;
     } else {
-      time = agent.parameters.eventTime;
-    }
-    agent.add(i18n.__('Looking for events'));
-    const fetched = fetchEvents(time);
-    return fetched.then((res) => {
-      console.log('Fetched');
-      // return top 3 events
-      if (res.length < 1) {
-        agent.add(i18n.__('No events found'));
+      let time;
+      if (agent.context.get('time').parameters.time !== '') {
+        ({ time } = agent.context.get('time').parameters);
       } else {
-        agent.add(`${i18n.__('Top 3 events')}: `);
-        let i;
-        let card;
-        for (i = 0; i < res.length; i += 1) {
-          card = new EventCard(res[i]);
-          agent.add(card);
-        }
+        time = agent.parameters.eventTime;
       }
-    });
+      agent.add(i18n.__('Looking for events'));
+      const fetched = fetchEvents(time);
+      return fetched.then((res) => {
+        console.log('Fetched');
+        // return top 3 events
+        if (res.length < 1) {
+          agent.add(i18n.__('No events found'));
+        } else {
+          agent.add(`${i18n.__('Top 3 events')}: `);
+          let i;
+          let card;
+          for (i = 0; i < res.length; i += 1) {
+            card = new EventCard(res[i]);
+            agent.add(card);
+          }
+        }
+      });
+    }
   },
 };
