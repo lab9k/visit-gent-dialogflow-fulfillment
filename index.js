26d5ab455c48477795afe281257df820955e1386
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-05 16:06:21 +0200

    solve language bug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const dialogflow = require('dialogflow');
 const express = require('express');
 const i18n = require('i18n');
 const bodyParser = require('body-parser');
@@ -5,12 +6,28 @@ const dotenv = require('dotenv');
 const fulfillment = require('./src/fulfillment');
 const knowledge = require('./src/knowledge');
 
-
-/* const { fetchAttractions, fetchEvents } = require('./api/SparqlApi');
-const AttractionCard = require('./src/models/AttractionCard');
-const EventCard = require('./src/models/EventCard');
-const requestPromise = require('request-promise'); */
-
+async function detectIntent() {
+  const client = new dialogflow.v2beta1.SessionsClient();
+  const formattedSession = client.sessionPath('visit-gent-qghbjt', '5');
+  const queryInput = {
+    text: {
+      text: 'Bonjour',
+      languageCode: 'fr',
+    },
+  };
+  const request = {
+    session: formattedSession,
+    queryInput,
+  };
+  client.detectIntent(request)
+    .then((responses) => {
+      const response = responses[0];
+      console.log(response.queryResult.fulfillmentText);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 const app = express();
 app.use(bodyParser.json());
@@ -27,53 +44,8 @@ i18n.configure({
   defaultLocale: 'en',
 });
 
-/* function requestLocation(location) {
-  const apiLocation = location.replace(/ /g, '+');
-  return requestPromise(`https://eu1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_API_KEY}&q=${apiLocation}&format=json`)
-    .catch(e => console.log(e.message));
-} */
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-
-  /* const fetched = fetchEvents('2019-07-18T12:00:00+02:00');
-  return fetched.then((res) => {
-    let i;
-    let card;
-    for (i = 0; i < res.length; i += 1) {
-      card = new EventCard(res[i]);
-      console.log(card);
-    }
-  });
-
-  const fetchedLocation = requestLocation('graslei');
-  const fetchedAttractions = fetchAttractions('Cafés');
-  Promise.all([fetchedLocation, fetchedAttractions]).then((res) => {
-    if (res[0] !== undefined) {
-      const location = JSON.parse(res[0])[0];
-      if (!location.display_name.includes('Ghent')) {
-        console.log('Location not found in Ghent');
-      } else {
-        const latitude = parseFloat(location.lat);
-        const longitude = parseFloat(location.lon);
-        const attractions = res[1];
-        let i;
-        const counter = 0;
-        const radius = 0.002;
-        for (i = 0; (i < attractions.length && counter < 10); i += 1) {
-          const loc = attractions[i].asWKT.value.replace('POINT(', '').replace(')', '').split(' ');
-          if ((parseFloat(loc[0]) < (longitude + radius)
-      && parseFloat(loc[0]) > (longitude - radius))
-      && (parseFloat(loc[1]) < (latitude + radius)
-      && parseFloat(loc[1]) > (latitude - radius))) {
-            console.log(attractions[i].name);
-            // counter += 1;
-            // console.log(new AttractionCard(attractions[i]));
-          }
-        }
-        console.log(counter);
-      }
-    }
-  }); */
+  detectIntent();
 });
