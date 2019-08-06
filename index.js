5d620fb6ce72d99e47e4c3d32b080c54db663eea
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-06 10:33:19 +0200

    auth

--- index.js
+++ index.js
@@ -6,39 +6,6 @@ const dotenv = require('dotenv');
 const fulfillment = require('./src/fulfillment');
 const knowledge = require('./src/knowledge');
 
-
-async function detectIntent() {
-  // const privateKey = (process.env.NODE_ENV === 'production') ? JSON.parse(process.env.DIALOGFLOW_PRIVATE_KEY) : process.env.DIALOGFLOW_PRIVATE_KEY;
-  const privateKey = process.env.DIALOGFLOW_PRIVATE_KEY;
-  const clientEmail = process.env.DIALOGFLOW_CLIENT_EMAIL;
-  const config = {
-    credentials: {
-      private_key: privateKey,
-      client_email: clientEmail,
-    },
-  };
-  const client = new dialogflow.SessionsClient(config);
-  const formattedSession = client.sessionPath('visit-gent-qghbjt', '5');
-  const queryInput = {
-    text: {
-      text: 'Bonjour',
-      languageCode: 'fr',
-    },
-  };
-  const request = {
-    session: formattedSession,
-    queryInput,
-  };
-  client.detectIntent(request)
-    .then((responses) => {
-      const response = responses[0];
-      console.log(response.queryResult.fulfillmentText);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -54,8 +21,54 @@ i18n.configure({
   defaultLocale: 'en',
 });
 
+const LANGUAGE_CODE = 'fr';
+
+class DialogFlow {
+  constructor(projectId) {
+    this.projectId = projectId;
+
+    const privateKey = (process.env.NODE_ENV === 'production') ? JSON.parse(process.env.DIALOGFLOW_PRIVATE_KEY) : process.env.DIALOGFLOW_PRIVATE_KEY;
+    const clientEmail = process.env.DIALOGFLOW_CLIENT_EMAIL;
+    const config = {
+      credentials: {
+        private_key: privateKey,
+        client_email: clientEmail,
+      },
+    };
+
+    this.sessionClient = new dialogflow.SessionsClient(config);
+  }
+
+  async sendTextMessageToDialogFlow(textMessage, sessionId) {
+    // Define session path
+    const sessionPath = this.sessionClient.sessionPath(this.projectId, sessionId);
+    // The text query request.
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: textMessage,
+          languageCode: LANGUAGE_CODE,
+        },
+      },
+    };
+    try {
+      const responses = await this.sessionClient.detectIntent(request);
+      console.log('DialogFlow.sendTextMessageToDialogFlow: Detected intent');
+      console.log(responses[0].queryResult.fulfillmentText);
+      return responses;
+    } catch (err) {
+      console.error('DialogFlow.sendTextMessageToDialogFlow ERROR:', err);
+      throw err;
+    }
+  }
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  detectIntent();
+  console.log(process.env.DIALOGFLOW_PRIVATE_KEY);
+  console.log(process.env.DIALOGFLOW_CLIENT_EMAIL);
+  // detectIntent();
+  const d = new DialogFlow('visit-gent-qghbjt');
+  d.sendTextMessageToDialogFlow('Bonjour', '1');
 });
