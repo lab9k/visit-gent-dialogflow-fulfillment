9da3357f42c686b9dcfe1051ed16a794e758bbd4
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-07 09:23:03 +0200

    fix card

--- src/webhook.js
+++ src/webhook.js
@@ -164,6 +164,7 @@ module.exports = {
                         },
                       ],
                     };
+                    responseJSONCard.message.attachment.payload.elements.push(responseJSON);
                   }
                 });
 
