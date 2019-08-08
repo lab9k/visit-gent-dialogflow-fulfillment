e2f5df717f7ff16e3a40ce8f19fc20c1592c8aa8
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-08 09:04:51 +0200

    multilang

--- src/intents/bot.activities.js
+++ src/intents/bot.activities.js
@@ -6,8 +6,8 @@ const i18n = require('i18n');
 module.exports = {
   key: 'bot.activities',
   handler(agent) {
-    agent.add(new Suggestion(i18n.__('Events')));
-    agent.add(new Suggestion(i18n.__('Attractions')));
+    agent.add(new Suggestion(i18n.__('Event')));
+    agent.add(new Suggestion(i18n.__('AttractionsShort')));
     agent.add(`${i18n.__('What kind of activity would you like to do')}?`);
   },
 };
